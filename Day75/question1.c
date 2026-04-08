c9d45c5af73edd6725e285461c9fda5658f69424
shashdashcash-oss <281527480+shashdashcash-oss@users.noreply.github.com>
2026-04-08 01:38:53 +0000

    Day 75 - Q1: Largest Subarray with Zero Sum

---
+++ Day75/question1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[1000];
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    int prefix[2000], index[2000], sz = 0;
+    int sum = 0, maxLen = 0;
+
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+        if (sum == 0) maxLen = i + 1;
+
+        int found = 0;
+        for (int j = 0; j < sz; j++) {
+            if (prefix[j] == sum) {
+                int len = i - index[j];
+                if (len > maxLen) maxLen = len;
+                found = 1; break;
+            }
+        }
+        if (!found) { prefix[sz] = sum; index[sz] = i; sz++; }
+    }
+
+    printf("%d\n", maxLen);
+    return 0;
+}
