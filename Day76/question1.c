f6df61c366581566cdeaf1e9c9d59acb9f3d22e1
shashdashcash-oss <281527480+shashdashcash-oss@users.noreply.github.com>
2026-04-09 14:25:31 +0000

    Day 76 - Q1: Count Connected Components (Undirected Graph)

---
+++ Day76/question1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node { int vertex; struct Node* next; };
+
+struct Node* addNode(struct Node* head, int v) {
+    struct Node* n = (struct Node*)malloc(sizeof(struct Node));
+    n->vertex = v; n->next = NULL;
+    if (!head) return n;
+    struct Node* t = head;
+    while (t->next) t = t->next;
+    t->next = n;
+    return head;
+}
+
+void dfs(int node, struct Node* adj[], int visited[]) {
+    visited[node] = 1;
+    struct Node* t = adj[node];
+    while (t) { if (!visited[t->vertex]) dfs(t->vertex, adj, visited); t = t->next; }
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+    struct Node* adj[105] = {NULL};
+    for (int i = 0; i < m; i++) {
+        int u, v; scanf("%d %d", &u, &v);
+        adj[u] = addNode(adj[u], v);
+        adj[v] = addNode(adj[v], u);
+    }
+    int visited[105] = {0};
+    int count = 0;
+    for (int i = 1; i <= n; i++) if (!visited[i]) { dfs(i, adj, visited); count++; }
+    printf("%d\n", count);
+    return 0;
+}
