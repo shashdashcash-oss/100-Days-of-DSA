ae5b64a87f41e7d0f1fe65a420cfc0935401bf04
shashdashcash-oss <281527480+shashdashcash-oss@users.noreply.github.com>
2026-04-28 16:11:41 +0000

    Day 95 - Q1: Bucket Sort (Float Values 0-1)

---
+++ Day95/question1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void insertionSortBucket(float arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        float key = arr[i]; int j = i - 1;
+        while (j >= 0 && arr[j] > key) { arr[j + 1] = arr[j]; j--; }
+        arr[j + 1] = key;
+    }
+}
+
+void bucketSort(float arr[], int n) {
+    float** buckets = (float**)malloc(n * sizeof(float*));
+    int* sizes = (int*)calloc(n, sizeof(int));
+    for (int i = 0; i < n; i++) buckets[i] = (float*)malloc(n * sizeof(float));
+
+    for (int i = 0; i < n; i++) {
+        int idx = (int)(n * arr[i]);
+        buckets[idx][sizes[idx]++] = arr[i];
+    }
+    for (int i = 0; i < n; i++) insertionSortBucket(buckets[i], sizes[i]);
+
+    int index = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < sizes[i]; j++) arr[index++] = buckets[i][j];
+        free(buckets[i]);
+    }
+    free(buckets); free(sizes);
+}
+
+int main() {
+    int n; scanf("%d", &n);
+    float arr[n];
+    for (int i = 0; i < n; i++) scanf("%f", &arr[i]);
+    bucketSort(arr, n);
+    for (int i = 0; i < n; i++) printf("%.2f ", arr[i]);
+    printf("\n");
+    return 0;
+}
